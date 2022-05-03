3bcfcf37e8c8a2b7532f2a2746852e0f85d8eb12
alejo16 <alejandrovalencia2011@gmail.com>
2022-05-03 10:38:02 -0600

    cambio de color

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,13 +1,13 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     return (
         <>
             <div className="nav-arriba">
                 <div className="nav-btn">
-                    <h2>Logo</h2>
+                    <Link to="/">Alejandro V.N </Link>
                     <button onClick={() => props.cambio()}><i className={props.iconFlecha} aria-hidden="true"></i></button>
                 </div>
-                <div className={"combobox "+ props.abrir}>
+                <div className={"combobox " + props.abrir}>
                     <button onClick={() => props.combo()}><i className={props.iconFlechaCombo} aria-hidden="true"></i></button>
                     <ul>
                         <li><Link to="/" onClick={() => props.combo()}>Home </Link></li>
@@ -17,12 +17,16 @@ const NavBar = (props) => {
                 </div>
                 <button className="button-h" onClick={() => props.cambioBtn()}><i className={props.icon} aria-hidden="true"></i></button>
             </div>
-            <div className={"nav " + props.active +" "+props.activeMovile}>
+            <div className={"nav " + props.active + " " + props.activeMovile}>
                 <ul>
-                    <li><Link to="/" onClick={() => props.cambioBtn()}>Home <i className="fa fa-home" aria-hidden="true"></i> </Link> </li>
-                    <li><Link to="/about" onClick={() => props.cambioBtn()}>About Us <i className="fa fa-address-card-o" aria-hidden="true"></i></Link> </li>
-                    <li><Link to="/contact" onClick={() => props.cambioBtn()}>Contact <i class="fa fa-address-book-o" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/" onClick={() => props.cambioBtn()}>Home <i className="fa fa-home click" aria-hidden="true"><span className="tooltip">Home</span></i> </Link> </li>
+                    <li><Link to="/about" onClick={() => props.cambioBtn()}>About Us <i className="fa fa-address-card-o click" aria-hidden="true"><span className="tooltip">About</span></i></Link> </li>
+                    <li><Link to="/contact" onClick={() => props.cambioBtn()}>Contact <i class="fa fa-address-book-o click" aria-hidden="true"><span className="tooltip">Contact</span></i></Link></li>
                 </ul>
+                <div className={"footer "+ props.active}>
+                    <p>Author: Alejandro Valencia</p>
+                    <p><a href="mailto:hege@example.com">alejandrovalencia2011@gmail.com</a></p>
+                </div>
             </div>
         </>
     );
